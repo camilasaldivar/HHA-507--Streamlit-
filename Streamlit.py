8733ce81b45f3a6a8aaa893b5658b58f3ab3bfb3
camilasaldivar <91294234+camilasaldivar@users.noreply.github.com>
2021-12-14 20:21:30 -0500

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -72,13 +72,11 @@ st.markdown('The dataframe below shows information regarding hospitals')
 st.dataframe(df_hospital_2)
 
 st.subheader('Map of NY Hospital Locations')
-
 hospitals_ny_gps = hospitals_ny['location'].str.strip('()').str.split(' ', expand=True).rename(columns={0: 'Point', 1:'lon', 2:'lat'})  
 hospitals_ny_gps['lon'] = hospitals_ny_gps['lon'].str.strip('(')
 hospitals_ny_gps = hospitals_ny_gps.dropna()
 hospitals_ny_gps['lon'] = pd.to_numeric(hospitals_ny_gps['lon'])
 hospitals_ny_gps['lat'] = pd.to_numeric(hospitals_ny_gps['lat'])
-
 st.map(hospitals_ny_gps)
 
 st.subheader('Hospital Type')
